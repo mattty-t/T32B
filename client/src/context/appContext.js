6efc1fff8c6e26d70af87fffaa02f52322f2701c
mattty-t <mattty.tran@gmail.com>
2022-01-10 02:00:18 +1000

    display alert file

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,6 +1,7 @@
 import React, { useReducer, useContext } from 'react';
-
 import reducer from './reducer';
+
+import { DISPLAY_ALERT } from './actions';
 
 const initialState = {
   isLoading: false,
@@ -13,8 +14,13 @@ const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const displayAlert = () => {
+    dispatch({ type: DISPLAY_ALERT });
+  };
   return (
-    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ ...state, displayAlert }}>
+      {children}
+    </AppContext.Provider>
   );
 };
 
