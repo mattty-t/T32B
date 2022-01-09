51168dc9aa51968498bcb45ba4208113c6f7a30c
mattty-t <mattty.tran@gmail.com>
2022-01-09 23:00:36 +1000

    styling error page

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+import img from '../assets/images/not-found.svg';
+import Wrapper from '../assets/wrappers/ErrorPage';
+
 const Error = () => {
-  return <div></div>;
+  return (
+    <Wrapper className="full-page">
+      <div>
+        <img src={img} alt="not found" />
+        <h3> Page not found!</h3>
+        <p>We can't seem to find the page you are looking for</p>
+      </div>
+      <Link to="/">back home</Link>
+    </Wrapper>
+  );
 };
 
 export default Error;
