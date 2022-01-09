81a977103925de2fc495aee3e1f1d7568e206b69
mattty-t <mattty.tran@gmail.com>
2022-01-09 19:43:21 +1000

    editing landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -5,9 +5,21 @@ import React from 'react';
 
 const Landing = () => {
   return (
-    <div>
-      <h4>Landing Page</h4>
-    </div>
+    <main>
+      <nav>
+        <img src={logo} alt="job" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>Hello world</p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+      </div>
+      <img src={main} alt="job hunt" className="img main-img" />
+    </main>
   );
 };
 
