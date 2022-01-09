9a7f62e4a834378ac30fe1602ac3a082b7192c42
mattty-t <mattty.tran@gmail.com>
2022-01-09 19:29:35 +1000

    fix landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
+import Landing from './pages/Landing';
+
 function App() {
-  return <h1>hello world</h1>;
+  return (
+    <div>
+      <h1>hello world</h1>
+      <Landing />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/pages/Landing.js
@@ -0,0 +1,14 @@
+import logo from '../assets/images/logo.svg';
+import main from '../assets/images/main.svg';
+
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div>
+      <h4>Landing Page</h4>
+    </div>
+  );
+};
+
+export default Landing;
