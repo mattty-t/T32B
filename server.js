dfe73575967937e3ac184c8d63c84eb8292f2046
Job Chow <jobitoalv@gmail.com>
2022-01-10 11:27:52 +1000

    notfound middleware

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 import express from 'express'
+
 const app = express()
+
+//middleware
+
+import notFoundMiddleware from './middleware/not-found.js'
 
 app.get('/', (req, res) => {
   res.send('Welcome!')
 })
 
+app.use(notFoundMiddleware)
 
 const port = process.env.PORT || 5000
 
