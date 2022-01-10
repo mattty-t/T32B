c4d05e4ebc859510b986edb6945900e33f4006cc
mattty-t <mattty.tran@gmail.com>
2022-01-11 00:45:20 +1000

    set up jwt token and lifetime

--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import validator from 'validator';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -41,12 +43,14 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-UserSchema.methods.createJWT = function () {
-  console.log(this);
-};
-
 UserSchema.pre('save', async function () {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ id: this._id }, 'process.env.JWT_SECRET', {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+};
 export default mongoose.model('User', UserSchema);
