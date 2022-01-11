ac53f4be234a0fec760057496b78d01161be80c6
Job Chow <jobitoalv@gmail.com>
2022-01-11 10:02:23 +1000

    setting up NPM Package

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 const app = express();
 import dotenv from 'dotenv';
 import 'express-async-errors';
+import morgan from 'morgan';
 
 dotenv.config();
 
@@ -17,6 +18,9 @@ import jobsRouter from './routes/jobRoutes.js';
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
 
+if(process.env.NODE-ENV !== 'production'){
+  app.use(morgan('dev'))
+}
 app.use(express.json());
 
 app.get('/', (req, res) => {
