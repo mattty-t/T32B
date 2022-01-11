7b2ace8c0174f4922b16268b6938a21b90dc9b9a
Job Chow <jobitoalv@gmail.com>
2022-01-11 11:25:02 +1000

    front end db setup

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -16,7 +16,7 @@ const Register = () => {
 
   //global state and useNavigate
 
-  const { user, isLoading, showAlert, displayAlert, registerUser } =
+  const { user, isLoading, showAlert, displayAlert, registerUser, loginUser } =
     useAppContext();
 
   const toggleMember = () => {
@@ -37,7 +37,7 @@ const Register = () => {
     }
     const currentUser = { name, email, password };
     if (isMember) {
-      console.log('already a member');
+      loginUser(currentUser)
     } else {
       registerUser(currentUser);
     }
