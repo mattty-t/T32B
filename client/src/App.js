fae2896621df60390881d46c816b3b4ef5b50363
Job Chow <jobitoalv@gmail.com>
2022-01-11 13:09:17 +1000

    created routes for pages

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Dashboard</div>} />
+        <Route path="/">
+          <Route path="stats" element={<Stats />} />
+          <Route path="alljobs" element={<AllJobs />} />
+          <Route path="addjob" element={<AddJob />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
         <Route path="/register" element={<Register />} />
         <Route path="/landing" element={<Landing />} />
         <Route path="*" element={<Error />} />
@@ -14,5 +19,7 @@ function App() {
     </BrowserRouter>
   );
 }
+
+        
 
 export default App;
--- client/src/pages/Dashboard/AllJobs.js
+++ client/src/pages/Dashboard/AllJobs.js
@@ -1,5 +1,5 @@
 const AllJobs = () => {
-  return <h1>All Jobs Page</h1> 
+  return <h1>AllJobs Page</h1>
 }
 
 export default AllJobs
