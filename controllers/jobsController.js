9624e66cfcef63b80fd215cf59a1831d545ac745
mattty-t <mattty.tran@gmail.com>
2022-01-14 03:16:35 +1000

    add job delete functionionality

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -50,7 +50,18 @@ const updateJob = async (req, res) => {
 };
 
 const deleteJob = async (req, res) => {
-  res.send('Delete Job ');
+  const { id: jobId } = req.params;
+  const job = await Job.findOne({ _id: jobId });
+
+  if (!job) {
+    throw new NotFoundError(`No job with id: ${jobId}`);
+  }
+
+  checkPermissions(req.user, job.createdBy);
+
+  await job.remove();
+
+  res.status(StatusCodes.OK).json({ msg: 'Success! Job removed' });
 };
 
 // const updateJob = async (req, res) => {
