14cd51d169c9be6bb88468617709f8750270d577
mattty-t <mattty.tran@gmail.com>
2022-01-14 00:43:46 +1000

    add seteditjob functionality

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -26,6 +26,7 @@ import {
   CREATE_JOB_ERROR,
   GET_JOBS_BEGIN,
   GET_JOBS_SUCCESS,
+  SET_EDIT_JOB,
 } from './actions';
 
 const token = localStorage.getItem('token');
@@ -259,7 +260,11 @@ const AppProvider = ({ children }) => {
   };
 
   const setEditJob = (id) => {
-    console.log(`set edit job : $${id}`);
+    dispatch({ type: SET_EDIT_JOB, payload: { id } });
+  };
+
+  const editJob = () => {
+    console.log('edit job');
   };
 
   const deleteJob = (id) => {
@@ -282,6 +287,7 @@ const AppProvider = ({ children }) => {
         getJobs,
         setEditJob,
         deleteJob,
+        editJob,
       }}
     >
       {children}
