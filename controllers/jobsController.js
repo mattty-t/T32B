1269d6d9ec0cfea8e17e05043d950b7b187101d1
Job Chow <jobitoalv@gmail.com>
2022-01-14 16:51:58 +1000

    filter sort

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -21,6 +21,36 @@ const createJob = async (req, res) => {
 };
 
 const getAllJobs = async (req, res) => {
+  const { status, jobType, sort, search } = req.query
+  const queryObject = {
+    createdBy: req.user.userId,
+  }
+
+  if (status && status !== 'all') {
+    queryObject.status = status
+  }
+  if (jobType && jobType !== 'all') {
+    queryObject.jobType = jobType
+  }
+  if (search) {
+    queryObject.position = { $regex: search, $options: 'i' }
+  }
+
+  let result = Job.find(queryObject)
+
+  if (sort === 'latest') {
+    result = result.sort('-createdAt')
+  }
+  if (sort === 'oldest') {
+    result = result.sort('createdAt')
+  }
+  if (sort === 'a-z') {
+    result = result.sort('position')
+  }
+  if (sort === 'z-a') {
+    result = result.sort('-position')
+  }
+
   const jobs = await Job.find({ createdBy: req.user.userId });
   res
     .status(StatusCodes.OK)
